4b85c079a48b77c7cd9ed375e3abbc0a7950ee35
mdavic0 <mdavico@fi.uba.ar>
2024-10-02 00:15:16 -0300

    prepearing for db connection

---
+++ src/app/seed/route.js
@@ -0,0 +1,41 @@
+import { db } from '@vercel/postgres';
+
+const client = await db.connect();
+const counter = { name: 'WeCount', value: 0 };
+
+
+async function seedCounter() {
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS counter (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        value INTEGER NOT NULL DEFAULT 0,
+        UNIQUE(name)
+    );
+  `;
+
+  const insertedRevenue = await Promise.all(
+    counter.map(
+      (c) => client.sql`
+        INSERT INTO counter (name, value)
+        VALUES (${c.name}, ${c.value})
+        ON CONFLICT (name) DO NOTHING;
+      `,
+    ),
+  );
+
+  return insertedRevenue;
+}
+
+export async function GET() {
+  try {
+    await client.sql`BEGIN`;
+    await seedCounter();
+    await client.sql`COMMIT`;
+
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    await client.sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
+  }
+}
